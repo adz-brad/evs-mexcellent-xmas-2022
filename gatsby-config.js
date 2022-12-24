636e61eaaae6e4886d1770e0e2c542911c7a1c8d
Brad S <brad@adrenalizedigital.ca>
2022-12-24 08:35:43 -0600

    update

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,16 +10,6 @@ module.exports = {
     siteUrl: `https://evsmexcellentxmas.netlify.app`,
   },
   plugins: [
-    `gatsby-plugin-image`,
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
-      },
-    },
     'gatsby-plugin-postcss',
-    `gatsby-transformer-sharp`,
-    `gatsby-plugin-sharp`,
   ],
 }
